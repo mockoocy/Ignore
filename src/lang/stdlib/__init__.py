cb77e1ca1a360348c183039ed02f808db1e6b39e
pmockoocy <pawel.mocko@s2innovation.com>
2024-04-01 21:00:15 +0200

    2 spaces -> 4 spaces

--- src/lang/stdlib/__init__.py
+++ src/lang/stdlib/__init__.py
@@ -2,19 +2,19 @@ from inspect import getmembers
 import contextlib
 import io
 modules =[
-  "builtins",
-  "collections"
+    "builtins",
+    "collections"
 ]
 
 """
-  defition of built in variable declarations and function definitions
+    defition of built in variable declarations and function definitions
 """
 
 def get_all_members(modulename: str):
-  return __import__(modulename).__dict__
+    return __import__(modulename).__dict__
 
 
 with contextlib.redirect_stdout(io.StringIO()):
-  builtins = {}
-  for module in modules:
-    builtins.update(get_all_members(module))
+    builtins = {}
+    for module in modules:
+        builtins.update(get_all_members(module))
