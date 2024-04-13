532f559ea2ad47b017f49cc9b4e0be7cee5e2f12
pmockoocy <pawel.mocko@s2innovation.com>
2024-04-13 15:49:28 +0200

    added syntax error handling

--- src/lang/ErrorListener.py
+++ src/lang/ErrorListener.py
@@ -1,7 +1,7 @@
 from antlr4.error.ErrorListener import ErrorListener
 
 
-class MyErrorListener(ErrorListener):
+class IgnoreErrorListener(ErrorListener):
     def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
         raise Exception("ERROR: when parsing line %d column %d: %s\n" % \
                         (line, column, msg))
