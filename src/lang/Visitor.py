c59e8053ea521b55583c73b29c3a570e4bfcf216
pmockoocy <pawel.mocko@s2innovation.com>
2024-04-14 19:03:19 +0200

    postempy

--- src/lang/Visitor.py
+++ src/lang/Visitor.py
@@ -14,50 +14,9 @@ class Visitor(ignoreParserVisitor):
 
     @override
     def visitLiteral(self, ctx: ignoreParser.LiteralContext):
-        if ctx.LITERAL_STRING() is not None:
-            raw_string = str(ctx.LITERAL_STRING())
-            return raw_string[1:-1]
+        ctx.evaluate()
 
-        elif ctx.LITERAL_INT() is not None:
-            return int(str(ctx.LITERAL_INT()))
-
-        elif ctx.LITERAL_FLOAT() is not None:
-            return float(str(ctx.LITERAL_FLOAT()))
-
-        elif ctx.LITERAL_BOOL() is not None:
-            bool_str = str(ctx.LITERAL_BOOL())
-            if bool_str == "True":
-                return True
-            elif bool_str == "False":
-                return False
-
-        else:
-            raise NotImplementedError("Unsupported literal type")
-
-    @override
-    def visitProgram(self, ctx: ignoreParser.ProgramContext):
-        return self.visitChildren(ctx)
-
-    @override
-    def visitExpr(self, ctx: ignoreParser.ExprContext):
-        return ctx.evaluate()
 
     @override
     def visitFunctionCall(self, ctx: ignoreParser.FunctionCallContext):
         return ctx.evaluate()
-    
-    @override
-    def visitWrapped_expr(self, ctx: ignoreParser.Wrapped_exprContext):
-        return super().visitWrapped_expr(ctx)
-
-    @override
-    def visitBlock(self, ctx: ignoreParser.BlockContext):
-        return super().visitBlock(ctx)
-
-    @override
-    def visitCondition(self, ctx: ignoreParser.ConditionContext):
-        return super().visitCondition(ctx)
-
-    @override
-    def visitControl_statement(self, ctx: ignoreParser.Control_statementContext):
-        return super().visitControl_statement(ctx)
--- src/lang/utils/VariableInfo.py
+++ src/lang/utils/VariableInfo.py
@@ -1,11 +1,12 @@
 from dataclasses import dataclass
 from typing import Any
-
+from Parser import Parser
 @dataclass
 class VariableInfo[T]:
     value: T
     type: str = "Any"
     scope: int = 0 # deeper we get, bigger it gets
+    expression: Parser.ExprContext
 
     def __call__(self, *args: Any, **kwargs: Any) -> Any:
         if (self.type in ("Any", "Function")):
